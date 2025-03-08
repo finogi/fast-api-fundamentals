74efb4aa6e97cb8b4ca154d0c1ce27dcf41b1ad9
finogi <finogi@proton.me>
2025-03-08 15:36:58 +0530

    Test

---
+++ app/test1.py
@@ -0,0 +1,5 @@
+from test2 import calculator
+
+multiply_values = calculator(10, 20)
+
+print(multiply_values)
---
+++ app/test2.py
@@ -0,0 +1,4 @@
+def calculator(a, b):
+    def multiply(a, b):
+        return a * b
+    return multiply(a,b)
